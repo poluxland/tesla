705c83659ba332599399be15c8cec9a9994a8903
Jose Jerez <joseignaciojerez@gmail.com>
2022-12-17 11:04:00 -0300

    new tables and printing

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,7 @@
 class Cliente < ApplicationRecord
   validates_uniqueness_of :rut, :message => "ya registrado"
+  validates :rut, format: { with: /\b\d?\d.\d\d\d.\d\d\d-[K|0-9]/ }
   has_many :sucursale
+
 end
+
