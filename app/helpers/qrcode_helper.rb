d7406885015fd38709dd478c0432e42afd09c452
Jose Jerez <joseignaciojerez@gmail.com>
2022-12-19 19:41:56 -0300

    add qr code

---
+++ app/helpers/qrcode_helper.rb
@@ -0,0 +1,16 @@
+module QrcodeHelper
+  require 'rqrcode'
+
+  def render_qr_code text, size = 3
+    qrcode = RQRCode::QRCode.new(request.original_url)
+
+# NOTE: showing with default options specified explicitly
+svg = qrcode.as_svg(
+  color: "000",
+  shape_rendering: "crispEdges",
+  module_size: 11,
+  standalone: true,
+  use_path: true
+)
+  end
+end
