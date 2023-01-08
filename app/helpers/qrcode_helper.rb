ff566ba80b6f1b278db5da885d959a48b52fec4d
Jose Jerez <joseignaciojerez@gmail.com>
2023-01-08 16:45:39 -0300

    reporte with qr and stuff

--- app/helpers/qrcode_helper.rb
+++ app/helpers/qrcode_helper.rb
@@ -8,7 +8,20 @@ module QrcodeHelper
 svg = qrcode.as_svg(
   color: "000",
   shape_rendering: "crispEdges",
-  module_size: 7,
+  module_size: 5,
+  standalone: true,
+  use_path: true
+)
+  end
+
+  def render_qr_code_mini text, size = 3
+    qrcode = RQRCode::QRCode.new(request.original_url)
+
+# NOTE: showing with default options specified explicitly
+svg = qrcode.as_svg(
+  color: "000",
+  shape_rendering: "crispEdges",
+  module_size: 3,
   standalone: true,
   use_path: true
 )
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,7 +1,9 @@
 class Cliente < ApplicationRecord
   validates_uniqueness_of :rut, :message => "ya registrado"
+  validates_uniqueness_of :nombre, :message => "ya existe ese nombre"
   validates :rut, format: { with: /\A\b[1-9]?\d\d\d\d\d\d\d-[K|0-9]\z/ }
   has_many :sucursale, dependent: :destroy
+  validates :nombre, presence: true
 
 end
 
