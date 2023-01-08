760cc70bf4117492e0a67cabd9a9379043602012
Jose Jerez <joseignaciojerez@gmail.com>
2023-01-08 13:29:02 -0300

    add validations in reports

--- app/models/reporte.rb
+++ app/models/reporte.rb
@@ -2,4 +2,7 @@ class Reporte < ApplicationRecord
   belongs_to :cliente
   belongs_to :sucursale
   belongs_to :equipo
+  validates :incio, presence: true
+  validates :termino, presence: true
+  validates :servicio, presence: true
 end
